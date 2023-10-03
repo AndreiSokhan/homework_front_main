c00ebf864baec26b13f2889eb02edb2a9096c115
Andrei <sokhanpageproofs@gmail.com>
2023-10-03 22:57:59 +0300

    HW_1 in progress

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type UserType = {
+   id: number,
+   name: string,
+   age: number,
+   address: Array<AddressType>
+};
+
+type AddressType = {
+   street: string,
+   city: string,
+};
+
+type UserListPropsType = {
+   users: UserType[];
+};
+
+export const UserList = (props: UserListPropsType) => {
+   return (
+      <div id={'hw01-users'}>
+         <h2>User List:</h2>
+         <ul>
+            {props.users.map((user) => {
+               return(
+               // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+               <li key={user.id} id={`hw01-user-${user.id}`}>
+                  <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                  {user.address.street}, {user.address.city}
+               </li>
+               )
+            })}
+         </ul>
+      </div>
+   );
+};
