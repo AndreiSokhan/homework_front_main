956c89d693d768a0bd23ba53925379d5cda38218
Andrei <sokhanpageproofs@gmail.com>
2023-10-16 23:17:13 +0300

    HW_1 end

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,35 +1,31 @@
-import React from 'react';
+type UserListPropsType = {
+   users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
 
 type UserType = {
-   id: number,
-   name: string,
-   age: number,
-   address: Array<AddressType>
+   id: number
+   name: string
+   age: number
+   address: AddressType
 };
 
 type AddressType = {
-   street: string,
-   city: string,
+   street: string
+   city: string
 };
 
-type UserListPropsType = {
-   users: UserType[];
-};
 
 export const UserList = (props: UserListPropsType) => {
    return (
       <div id={'hw01-users'}>
          <h2>User List:</h2>
          <ul>
-            {props.users.map((user) => {
-               return(
-               // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+            {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
                <li key={user.id} id={`hw01-user-${user.id}`}>
                   <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
                   {user.address.street}, {user.address.city}
                </li>
-               )
-            })}
+            ))}
          </ul>
       </div>
    );
